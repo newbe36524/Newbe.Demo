47edf6e60dc72fd64d35a36da5571ba9346ef43b
newbe36524 <newbe36524@qq.com>
2020-04-13 23:02:54 +0800

    MultipleComponentStatus2

--- src/BlogDemos/Newbe.Rx/Newbe.RxWorld/Newbe.RxWorld/ServerStatusTest.cs
+++ src/BlogDemos/Newbe.Rx/Newbe.RxWorld/Newbe.RxWorld/ServerStatusTest.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Reactive.Linq;
 using System.Reactive.Subjects;
+using System.Threading.Tasks;
 using Xunit;
 using Xunit.Abstractions;
 
@@ -101,6 +102,77 @@ namespace Newbe.RxWorld
 
             void ShowCurrentServerStatus(bool status)
                 => _testOutputHelper.WriteLine($"server is health : {status}");
+        }
+
+        [Fact]
+        public void MultipleComponentStatus2()
+        {
+            var allComponentStatus = new[]
+            {
+                DatabaseStatus(),
+                RedisStatus(),
+                ElasticSearchStatus()
+            };
+
+            var serverStatus =
+                allComponentStatus
+                    .Aggregate(CombineAllComponentStatus)
+                    .DistinctUntilChanged()
+                    .Subscribe(ShowCurrentServerStatus);
+
+            serverStatus.Dispose();
+
+            static IObservable<bool>
+                CombineAllComponentStatus(IObservable<bool> statusOb1, IObservable<bool> statusOb2) =>
+                statusOb1.CombineLatest(statusOb2, CombineServerStatus);
+
+            static bool CombineServerStatus(bool status1, bool status2)
+                => status1 && status2;
+
+            void ShowCurrentServerStatus(bool status)
+                => _testOutputHelper.WriteLine($"server is health : {status}");
+
+            static IObservable<bool> DatabaseStatus()
+            {
+                return Observable.Interval(TimeSpan.FromSeconds(5))
+                    .Select(l => Observable.FromAsync(GetDatabaseStatus))
+                    .Concat();
+
+                Task<bool> GetDatabaseStatus()
+                {
+                    return Task.FromResult(false);
+                }
+            }
+
+            static IObservable<bool> RedisStatus()
+            {
+                return Observable.Interval(TimeSpan.FromSeconds(5))
+                    .Select(x => Observable.FromAsync(GetRedisStatus))
+                    .Concat()
+                    .Window(6)
+                    .Select(x => x.Any(a => a))
+                    .Concat();
+
+                Task<bool> GetRedisStatus()
+                {
+                    return Task.FromResult(false);
+                }
+            }
+
+            static IObservable<bool> ElasticSearchStatus()
+            {
+                return Observable.Interval(TimeSpan.FromSeconds(5))
+                    .Select(x => Observable.FromAsync(GetElasticSearchStatus))
+                    .Concat()
+                    .Window(6)
+                    .Select(x => x.Any(a => a))
+                    .Concat();
+
+                Task<bool> GetElasticSearchStatus()
+                {
+                    return Task.FromResult(false);
+                }
+            }
         }
     }
 }
