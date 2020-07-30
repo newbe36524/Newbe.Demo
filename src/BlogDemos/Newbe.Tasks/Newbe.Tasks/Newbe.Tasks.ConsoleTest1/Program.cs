788b8edf92d132172431c564d55af789d3238712
newbe36524 <472158246@qq.com>
2020-07-30 09:22:54 +0800

    TestConsole for TaskCompletionSource

---
+++ src/BlogDemos/Newbe.Tasks/Newbe.Tasks/Newbe.Tasks.ConsoleTest1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Newbe.Tasks.ConsoleTest1
+{
+    class Program
+    {
+        /**
+         * this console will exit if you create a file at d:/1.txt
+         */
+        static async Task Main(string[] args)
+        {
+            var tcs = new TaskCompletionSource<DateTime>();
+
+            // start a task to check file at d:/1.txt
+#pragma warning disable 4014
+            Task.Run(() =>
+            {
+                while (true)
+                {
+                    Thread.Sleep(TimeSpan.FromSeconds(1));
+                    if (File.Exists(Path.Combine("d:/", "1.txt")))
+                    {
+                        Console.WriteLine("file found!");
+                        tcs.SetResult(DateTime.Now);
+                        break;
+                    }
+
+                    Console.WriteLine($"{DateTime.Now:s} : file not found");
+                }
+            });
+#pragma warning restore 4014
+
+            Console.WriteLine("start to await task.");
+            await tcs.Task;
+            Console.WriteLine("console exit.");
+        }
+    }
+}
---
+++ src/BlogDemos/Newbe.Tasks/Newbe.Tasks/Newbe.Tasks.ConsoleTest2/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Newbe.Tasks.ConsoleTest2
+{
+    class Program
+    {
+        /**
+         * this console will exit if you create a file at d:/1.txt
+         */
+        static async Task Main(string[] args)
+        {
+            var tasks = Enumerable.Range(0, 10)
+                .Select(RunTask);
+
+            // start a task to check file at d:/1.txt
+#pragma warning disable 4014
+            Task.Run(() =>
+            {
+                while (true)
+                {
+                    Thread.Sleep(TimeSpan.FromSeconds(1));
+                    if (File.Exists(Path.Combine("d:/", "1.txt")))
+                    {
+                        Console.WriteLine("file found!");
+                        var items = new List<TaskItem<int>>();
+                        while (TaskQueue.TryDequeue(out var item))
+                        {
+                            items.Add(item);
+                        }
+
+                        Console.WriteLine($"items completed : {string.Join(",", items.Select(x => x.Input))}");
+                        foreach (var taskItem in items)
+                        {
+                            taskItem.Tcs.SetResult(true);
+                        }
+
+                        break;
+                    }
+
+                    Console.WriteLine($"{DateTime.Now:s} : file not found");
+                }
+            });
+#pragma warning restore 4014
+
+            await Task.WhenAll(tasks);
+        }
+
+        private static readonly ConcurrentQueue<TaskItem<int>> TaskQueue
+            = new ConcurrentQueue<TaskItem<int>>();
+
+        private static Task<bool> RunTask(int data)
+        {
+            var tcs = new TaskCompletionSource<bool>();
+            TaskQueue.Enqueue(new TaskItem<int>
+            {
+                Input = data,
+                Tcs = tcs
+            });
+            return tcs.Task;
+        }
+
+        public struct TaskItem<T>
+        {
+            public T Input { get; set; }
+            public TaskCompletionSource<bool> Tcs { get; set; }
+        }
+    }
+}
