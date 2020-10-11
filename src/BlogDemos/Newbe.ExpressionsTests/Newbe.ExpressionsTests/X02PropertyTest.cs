cb2fbb319cab36a4bb4a9f932a01fe1908b3a0a5
Newbe36524 <newbe36524@qq.com>
2020-10-11 22:51:43 +0800

    X03PropertyValidationTest2

---
+++ src/BlogDemos/Newbe.ExpressionsTests/Newbe.ExpressionsTests/X02PropertyTest.cs
@@ -0,0 +1,63 @@
+using System;
+using System.Diagnostics;
+using System.Linq.Expressions;
+using System.Reflection;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace Newbe.ExpressionsTests
+{
+    public class X02PropertyTest
+    {
+        private const int Count = 1_000_000;
+        private const int Diff = 100;
+
+        [SetUp]
+        public void Init()
+        {
+            _propertyInfo = typeof(Claptrap).GetProperty(nameof(Claptrap.Level));
+            Debug.Assert(_propertyInfo != null, nameof(_propertyInfo) + " != null");
+
+            var instance = Expression.Parameter(typeof(Claptrap), "c");
+            var levelProperty = Expression.Property(instance, _propertyInfo);
+            var levelP = Expression.Parameter(typeof(int), "l");
+            var addAssignExpression = Expression.AddAssign(levelProperty, levelP);
+            var lambdaExpression = Expression.Lambda<Action<Claptrap, int>>(addAssignExpression, instance, levelP);
+            // lambdaExpression should be as (Claptrap c,int l) =>  { c.Level += l; }
+            _func = lambdaExpression.Compile();
+        }
+
+        [Test]
+        public void RunReflection()
+        {
+            var claptrap = new Claptrap();
+            for (int i = 0; i < Count; i++)
+            {
+                var value = (int) _propertyInfo.GetValue(claptrap);
+                _propertyInfo.SetValue(claptrap, value + Diff);
+            }
+
+            claptrap.Level.Should().Be(Count * Diff);
+        }
+
+        [Test]
+        public void RunExpression()
+        {
+            var claptrap = new Claptrap();
+            for (int i = 0; i < Count; i++)
+            {
+                _func.Invoke(claptrap, Diff);
+            }
+
+            claptrap.Level.Should().Be(Count * Diff);
+        }
+
+        private PropertyInfo _propertyInfo;
+        private Action<Claptrap, int> _func;
+
+        public class Claptrap
+        {
+            public int Level { get; set; }
+        }
+    }
+}
---
+++ src/BlogDemos/Newbe.ExpressionsTests/Newbe.ExpressionsTests/X03PropertyValidationTest1.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics;
+using System.Linq.Expressions;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace Newbe.ExpressionsTests
+{
+    public class X03PropertyValidationTest1
+    {
+        private const int Count = 1000;
+
+        private static Func<CreateClaptrapInput, int, ValidateResult> _func;
+
+        [SetUp]
+        public void Init()
+        {
+            try
+            {
+                var method = typeof(X03PropertyValidationTest1).GetMethod(nameof(ValidateCore));
+                Debug.Assert(method != null, nameof(method) + " != null");
+                var pExp = Expression.Parameter(typeof(CreateClaptrapInput));
+                var minLengthPExp = Expression.Parameter(typeof(int));
+                var body = Expression.Call(method, pExp, minLengthPExp);
+                var expression = Expression.Lambda<Func<CreateClaptrapInput, int, ValidateResult>>(body,
+                    pExp,
+                    minLengthPExp);
+                _func = expression.Compile();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+        }
+
+        [Test]
+        public void Run()
+        {
+            for (int i = 0; i < Count; i++)
+            {
+                // test 1
+                {
+                    var input = new CreateClaptrapInput();
+                    var (isOk, errorMessage) = Validate(input);
+                    isOk.Should().BeFalse();
+                    errorMessage.Should().Be("missing Name");
+                }
+
+                // test 2
+                {
+                    var input = new CreateClaptrapInput
+                    {
+                        Name = "1"
+                    };
+                    var (isOk, errorMessage) = Validate(input);
+                    isOk.Should().BeFalse();
+                    errorMessage.Should().Be("Length of Name should be great than 3");
+                }
+
+                // test 3
+                {
+                    var input = new CreateClaptrapInput
+                    {
+                        Name = "yueluo is the only one dalao"
+                    };
+                    var (isOk, errorMessage) = Validate(input);
+                    isOk.Should().BeTrue();
+                    errorMessage.Should().BeNullOrEmpty();
+                }
+            }
+        }
+
+        public static ValidateResult Validate(CreateClaptrapInput input)
+        {
+            return _func.Invoke(input, 3);
+        }
+
+        public static ValidateResult ValidateCore(CreateClaptrapInput input, int minLength)
+        {
+            if (string.IsNullOrEmpty(input.Name))
+            {
+                return ValidateResult.Error("missing Name");
+            }
+
+            if (input.Name.Length < minLength)
+            {
+                return ValidateResult.Error($"Length of Name should be great than {minLength}");
+            }
+
+            return ValidateResult.Ok();
+        }
+
+
+        public class CreateClaptrapInput
+        {
+            [Required] [MinLength(3)] public string Name { get; set; }
+        }
+
+        public struct ValidateResult
+        {
+            public bool IsOk { get; set; }
+            public string ErrorMessage { get; set; }
+
+            public void Deconstruct(out bool isOk, out string errorMessage)
+            {
+                isOk = IsOk;
+                errorMessage = ErrorMessage;
+            }
+
+            public static ValidateResult Ok()
+            {
+                return new ValidateResult
+                {
+                    IsOk = true
+                };
+            }
+
+            public static ValidateResult Error(string errorMessage)
+            {
+                return new ValidateResult
+                {
+                    IsOk = false,
+                    ErrorMessage = errorMessage
+                };
+            }
+        }
+    }
+}
