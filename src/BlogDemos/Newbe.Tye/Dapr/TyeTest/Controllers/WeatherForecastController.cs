98e86aea3ca61cd1769b23930dab0c416caa1384
newbe36524 <newbe36524@qq.com>
2021-02-05 15:18:42 +0800

    Dapr

---
+++ src/BlogDemos/Newbe.Tye/Dapr/TyeTest/Controllers/WeatherForecastController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace TyeTest.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+        private readonly IConfiguration _configuration;
+        private readonly HttpClient _httpClient;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger,
+            IConfiguration configuration,
+            HttpClient httpClient)
+        {
+            _logger = logger;
+            _configuration = configuration;
+            _httpClient = httpClient;
+        }
+
+        [HttpGet]
+        public async Task<string> Get()
+        {
+            var serviceUri = _configuration.GetServiceUri("tyetest2");
+            Console.WriteLine(serviceUri);
+            var httpResponseMessage = await _httpClient.GetAsync($"{serviceUri}WeatherForecast");
+            var json = await httpResponseMessage.Content.ReadAsStringAsync();
+            return json;
+        }
+    }
+}
