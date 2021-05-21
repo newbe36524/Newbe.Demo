83c9d00cc84910c689e84cc53dd8b9d9cf2f65c8
newbe36524 <472158246@qq.com>
2021-05-21 10:12:36 +0800

    no js

--- src/BlogDemos/Newbe.Blazor/Newbe.Blazors.GithubReleaseMirror/Newbe.Blazors.GithubReleaseMirror/Pages/Background.cs
+++ src/BlogDemos/Newbe.Blazor/Newbe.Blazors.GithubReleaseMirror/Newbe.Blazors.GithubReleaseMirror/Pages/Background.cs
@@ -1,20 +1,53 @@
 ﻿using System;
+using System.Text.Json;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
 using Microsoft.JSInterop;
+using WebExtension.Net.WebRequest;
 
 namespace Newbe.Blazors.GithubReleaseMirror.Pages
 {
     public partial class Background
     {
-        // e.g. https://github.com/dapr/cli/releases/download/v1.1.0/dapr_windows_amd64.zip
-        private static readonly Regex ReleaseRegex = new Regex(
-            @"https://github.com/(?<author>\w+)/(?<repo>\w+)/releases/download/(?<tag>\S+)/(?<file>\S+)",
-            RegexOptions.Compiled, TimeSpan.FromMilliseconds(500));
-
-        [JSInvokable]
-        public static string GetGithubReleaseMirrorUrl(string source)
+        protected override async Task OnInitializedAsync()
         {
+            await base.OnInitializedAsync();
+
+            await WebExtension.WebRequest.OnBeforeRequest.AddListener(details =>
+            {
+                var d = (JsonElement)details;
+                var url = d.GetProperty("url").ToString();
+                var targetUrl = GetGithubReleaseMirrorUrl(url);
+
+                return new BlockingResponse
+                {
+                    RedirectUrl = targetUrl,
+                };
+            }, new RequestFilter
+            {
+                Urls = new[] {"*://github.com/*"},
+            }, new[]
+            {
+                OnBeforeRequestOptions.Blocking
+            });
+            // this opens index.html in the extension as a new tab when the background page is loaded
+            var extensionUrl = await WebExtension.Runtime.GetURL("index.html");
+            await WebExtension.Tabs.Create(new
+            {
+                url = extensionUrl
+            });
+        }
+
+        // e.g. https://github.com/dapr/cli/releases/download/v1.1.0/dapr_windows_amd64.zip
+        private static readonly Regex ReleaseRegex = new(
+            @"https://github.com/(?<author>\w+)/(?<repo>\w+)/releases/download/(?<tag>\S+)/(?<file>\S+)",
+            RegexOptions.Compiled, 
+            TimeSpan.FromMilliseconds(500));
+
+        private string GetGithubReleaseMirrorUrl(string source)
+        {
+            Logger.LogInformation("url: {Source}",source);
             if (string.IsNullOrEmpty(source))
             {
                 return source;
@@ -25,6 +58,7 @@ namespace Newbe.Blazors.GithubReleaseMirror.Pages
                 var match = ReleaseRegex.Match(source);
                 if (match.Success)
                 {
+                    Logger.LogInformation("match!");
                     // e.g. "https://github.com.cnpmjs.org/dapr/cli/releases/download/$($release.tag_name)/dapr_windows_amd64.zip"
                     var mirror =
                         $"https://github.com.cnpmjs.org/{match.Groups["author"]}/{match.Groups["repo"]}/releases/download/{match.Groups["tag"]}/{match.Groups["file"]}";
