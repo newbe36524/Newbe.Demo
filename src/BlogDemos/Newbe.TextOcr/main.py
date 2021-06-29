cdb430dc44fc277df91a64a0b0d07fb243d584dc
newbe36524 <472158246@qq.com>
2021-06-29 18:29:06 +0800

    mark region

--- src/BlogDemos/Newbe.TextOcr/main.py
+++ src/BlogDemos/Newbe.TextOcr/main.py
@@ -3,7 +3,8 @@ import cv2
 import imutils
 import numpy as np
 import matplotlib.pyplot as plt
-import pytesseract
+import tesserocr as tr
+from PIL import Image
 
 
 class ImgProcess:
@@ -25,6 +26,13 @@ class ImgProcess:
         self.img_item_dir = os.path.join(data_folder_name, "items")
         self.img_item_list = []
         self.img_region_item_marked_dir = os.path.join(data_folder_name, "item_marked")
+
+        self.img_item_sub_dir = os.path.join(data_folder_name, "items_sub")
+
+        self.img_item_sub_marked_dir = os.path.join(data_folder_name, "items_sub_marked")
+
+        self.img_ocr_marked_dir = os.path.join(data_folder_name, "img_ocr_marked")
+        self.img_regions_ocr_marked_dir = os.path.join(data_folder_name, "img_regions_ocr_marked")
 
     def load_source(self):
         self.img = cv2.imread(self.filepath)
@@ -69,13 +77,14 @@ class ImgProcess:
                 if region.shape[1] < 1000:
                     scale_percent = 1000 // region.shape[1] * 100
                 else:
-                    scale_percent = 1000 // region.shape[0] * 100
+                    scale_percent = 1000 // region.shape[1] * 100
                 width = int(region.shape[1] * scale_percent / 100)
                 height = int(region.shape[0] * scale_percent / 100)
-                dim = (width, height)
-                # resize image
-                resized = cv2.resize(region, dim, interpolation=cv2.INTER_AREA)
-                self.img_region_list.append(np.copy(resized))
+                if width > 0 and height > 0:
+                    dim = (width, height)
+                    # resize image
+                    resized = cv2.resize(region, dim, interpolation=cv2.INTER_AREA)
+                    self.img_region_list.append(np.copy(resized))
             else:
                 self.img_region_list.append(np.copy(region))
 
@@ -87,8 +96,8 @@ class ImgProcess:
             # save region marked file
             path = os.path.join(self.img_region_marked_dir, f"{self.filename_without_ext}.png")
             cv2.imwrite(path, img_region_marked_view)
-        plt.imshow(img_region_marked_view, cmap='gray')
-        plt.show()
+            plt.imshow(img_region_marked_view, cmap='gray')
+            plt.show()
         return len(self.img_region_list)
 
     def get_table(self):
@@ -183,18 +192,99 @@ class ImgProcess:
 
     def ocr(self):
         s = ""
-        # item_test_list = []
+        # item_text_list = []
         # for i in range(len(self.img_item_list)):
         #     item = self.img_item_list[i]
         #     out = pytesseract.image_to_string(item)
         #     out_t = out.strip()
         #     if len(out_t) != 0:
-        #         item_test_list.append(out_t)
+        #         item_text_list.append(out_t)
         #         # s = s + f"\nitem {i}:\n" + out_t
         #         s = s + f"\n" + out_t
+        item_text_list = []
+        sub_list = []
+        with tr.PyTessBaseAPI(path='C:\\Program Files\\Tesseract-OCR\\tessdata\\', lang='eng') as api:
+            api.SetPageSegMode(tr.PSM.SPARSE_TEXT)
+            for i in range(len(self.img_item_list)):
+                item = self.img_item_list[i]
+                pil_img = Image.fromarray(cv2.cvtColor(item, cv2.COLOR_BGR2RGB))
+                api.SetImage(pil_img)
+                item_sub_marked = np.copy(item)
+
+                boxes = api.GetComponentImages(tr.RIL.PARA, True)
+                # get text
+                # text = api.GetUTF8Text()
+                # print(text)
+                # iterate over returned list, draw rectangles
+                for (im, box, _, _) in boxes:
+                    x, y, w, h = box['x'], box['y'], box['w'], box['h']
+                    cv2.rectangle(item_sub_marked, (x, y), (x + w, y + h), color=(0, 0, 255))
+
+                path = os.path.join(self.img_item_sub_marked_dir, f"{self.filename_without_ext}_{i}.png")
+                cv2.imwrite(path, item_sub_marked)
+        for i in range(len(sub_list)):
+            path = os.path.join(self.img_item_sub_dir, f"{self.filename_without_ext}_{i}.png")
+            cv2.imwrite(path, sub_list[i])
         print(f"{self.filename_without_ext}============")
         print(s)
         print("============")
+
+    def ocr_whole(self):
+        with tr.PyTessBaseAPI(path='C:\\Program Files\\Tesseract-OCR\\tessdata\\', lang='eng') as api:
+            api.SetPageSegMode(tr.PSM.AUTO_OSD)
+            item = self.img
+            pil_img = Image.fromarray(cv2.cvtColor(item, cv2.COLOR_BGR2RGB))
+            api.SetImage(pil_img)
+            item_sub_marked = np.copy(item)
+
+            boxes = api.GetComponentImages(tr.RIL.BLOCK, True)
+            # get text
+            # text = api.GetUTF8Text()
+            # print(text)
+            # iterate over returned list, draw rectangles
+            for (im, box, _, _) in boxes:
+                x, y, w, h = box['x'], box['y'], box['w'], box['h']
+                cv2.rectangle(item_sub_marked, (x, y), (x + w, y + h), color=(0, 0, 255))
+
+            # out = pytesseract.image_to_string(~resized_afterd, lang='eng', config='--psm 3')
+            # out_t = out.strip()
+            # if len(out_t) != 0:
+            #     item_text_list.append(out_t)
+            #     # s = s + f"\nitem {i}:\n" + out_t
+            #     s = s + f"\n" + out_t
+            path = os.path.join(self.img_ocr_marked_dir, f"{self.filename_without_ext}.png")
+            cv2.imwrite(path, item_sub_marked)
+            plt.show()
+
+    def ocr_regions(self):
+        def pre_process_region(region):
+            # get rid of the color
+            pre = cv2.cvtColor(region, cv2.COLOR_BGR2GRAY)
+            # Otsu threshold
+            # pre = cv2.threshold(pre, 200, 255, cv2.THRESH_BINARY)[1]
+            return pre
+
+        with tr.PyTessBaseAPI(
+                path='C:\\Program Files\\Tesseract-OCR\\tessdata\\', lang='eng', psm=tr.PSM.AUTO,
+                oem=tr.OEM.LSTM_ONLY) as api:
+            api.SetPageSegMode(tr.PSM.SINGLE_COLUMN)
+            for i in range(len(self.img_region_list)):
+                item = pre_process_region(self.img_region_list[i])
+                pil_img = Image.fromarray(item)
+                api.SetImage(pil_img)
+                item_sub_marked = np.copy(item)
+
+                boxes = api.GetComponentImages(tr.RIL.BLOCK, True)
+                # get text
+                text = api.GetUTF8Text()
+                print(text)
+                # iterate over returned list, draw rectangles
+                for (im, box, _, _) in boxes:
+                    x, y, w, h = box['x'], box['y'], box['w'], box['h']
+                    cv2.rectangle(item_sub_marked, (x, y), (x + w, y + h), color=(0, 0, 255))
+
+                path = os.path.join(self.img_regions_ocr_marked_dir, f"{self.filename_without_ext}_{i}.png")
+                cv2.imwrite(path, item_sub_marked)
 
 
 if __name__ == "__main__":
@@ -207,6 +297,7 @@ if __name__ == "__main__":
         in_file = source_file
         img = ImgProcess(source_file)
         img.load_source()
+        img.ocr_whole()
         region_count = 0
         max_index = -1
         max_region_count = 0
@@ -221,6 +312,7 @@ if __name__ == "__main__":
             continue
         img.pre_process(thresh_modes[max_index])
         img.get_regions(True)
+        img.ocr_regions()
         img.get_table()
         img.crop_by_table_line()
         img.ocr()
